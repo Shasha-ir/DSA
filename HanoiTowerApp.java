c3d744e6fbaa81d72461920127c91191ace4ed74
Isuri - UOG0723006 <isurirajakaruna2002@gmail.com>
2025-02-05 13:02:03 +0530

    Add files via upload

---
+++ HanoiTowerApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Hanoi;
+
+/**
+ *
+ * @author Shasha
+ */
+public class HanoiTowerApp {
+    static int hanoiCount(int no_of_disks)
+    {
+        if (no_of_disks == 1) //move the single disk
+            return 1;
+        return 2 * hanoiCount(no_of_disks - 1)+ 1;
+    }
+    
+    public static void main (String args[]){
+        int noOfDisks = 5;
+        int no_of_steps;
+        no_of_steps = hanoiCount(noOfDisks);
+        System.out.println("Total number of steps : " + no_of_steps);
+    }
+}
